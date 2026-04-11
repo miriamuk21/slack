e9ff918eaf544d36be63b1c4143ae3e4ebc17c9e
mmss <miriamuk21@hotmail.com>
2026-04-10 22:19:03 -0600

    connect database mongodb

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 //Requere the Cron package
 var CronJob = require('cron').CronJob;
 
+//Requere the mongodb package
+const { MongoClient } = require('mongodb');
+const mongouri = "mongodb+srv://miriamsmedium:pelu212121@mediumapp.e4q9knm.mongodb.net/?appName=mediumapp"
+let joke;// the joke parameter will hold our message data
+
 // Require the Node Slack SDK package (github.com/slackapi/node-slack-sdk)
 const { WebClient, LogLevel } = require("@slack/web-api");
 
@@ -10,9 +15,28 @@ const client = new WebClient("xoxb-3687547720391-10858503901763-gxLHeUKT1jI4Ct44
 const channelId = "U03LK6P4ZPF";
 
 
-var job = new CronJob('53 00 * * SAT', function() {
+var job = new CronJob('23 04 * * SAT', function() {
  //OUR CODE FOR SENDING A MESSAGE
     (async () => {
+
+    // Create a new MongoClient
+      let mongoClient = new MongoClient(mongouri, {
+        useUnifiedTopology: true 
+        })
+        try {
+            // Connect the client to the server
+            await mongoClient.connect();
+            let db = mongoClient.db('slackapp')//set the reference to the connected slackapp database as db
+            
+            //get the joke data
+            let messagesCollection = db.collection('messages')// set the reference to the messages collection as messagesCollection
+            joke=await messagesCollection.findOne({ nextup: true }) // this line will find one document in the messages collection that has the nextup value set to true, and store this document and its content as parameter named joke
+    
+            
+        } catch (errormongo) {
+            console.error(errormongo);
+            }
+      
     try {
       // Call the chat.postMessage method using the WebClient
       const result = await client.chat.postMessage({
@@ -23,19 +47,19 @@ var job = new CronJob('53 00 * * SAT', function() {
               "type": "section",
               "text": {
                 "type": "mrkdwn",
-                "text": "* - Why did my Exotic Bird Startup Store fail?*"
+                "text": joke.setup
               }
             },
             {
               "type": "section",
               "text": {
                 "type": "mrkdwn",
-                "text": " - I didn't have enough seed money."
+                "text": joke.punch
               }
             },
             {
               "type": "image",
-              "image_url": "https://cdn.pixabay.com/photo/2017/08/09/15/32/jokers-2614901_1280.jpg",
+              "image_url": joke.image,
               "alt_text": "delicious tacos"
             }
           ]
