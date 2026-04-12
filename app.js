186b9269a186a42235f20f15d3e6635dd8e70936
mmss <miriamuk21@hotmail.com>
2026-04-11 18:46:17 -0600

    new cgpt code

--- app.js
+++ app.js
@@ -1,75 +1,50 @@
-//Requere the Cron package
 var CronJob = require('cron').CronJob;
-
-//Requere the mongodb package
 const { MongoClient } = require('mongodb');
-const mongouri = "mongodb+srv://miriamsmedium:pelu212121@mediumapp.e4q9knm.mongodb.net/slackapp?retryWrites=true"
-let joke;// the joke parameter will hold our message data
+const { WebClient } = require("@slack/web-api");
 
-// Require the Node Slack SDK package (github.com/slackapi/node-slack-sdk)
-const { WebClient, LogLevel } = require("@slack/web-api");
+const mongouri = process.env.MONGO_URI;
+const client = new WebClient(process.env.SLACK_TOKEN);
+const channelId = "C0AS7BGCPBP"; // real channel ID
 
-// WebClient insantiates a client that can call API methods
-const client = new WebClient("xoxb-3687547720391-10858503901763-gxLHeUKT1jI4Ct44XVogOTHs");
-// ID of the channel you want to send the message to
-const channelId = "C0AS7BGCPBP";
+var job = new CronJob(
+  '50 00 * * SUN',
+  async function () {
+    const mongoClient = new MongoClient(mongouri);
 
-
-var job = new CronJob('43 18 * * SAT', function() {}, null, true, 'America/Tegucigalpa');
- //OUR CODE FOR SENDING A MESSAGE
-    (async () => {
-
-    // Create a new MongoClient
-      let mongoClient = new MongoClient(mongouri, {
-        useUnifiedTopology: true 
-        })
-        try {
-            // Connect the client to the server
-            await mongoClient.connect();
-            let db = mongoClient.db('slackapp')//set the reference to the connected slackapp database as db
-            
-            //get the joke data
-            let messagesCollection = db.collection('messages')// set the reference to the messages collection as messagesCollection
-            joke=await messagesCollection.findOne({ nextup: true }) // this line will find one document in the messages collection that has the nextup value set to true, and store this document and its content as parameter named joke
-    
-            
-        } catch (errormongo) {
-            console.error(errormongo);
-            }
-      
     try {
-      // Call the chat.postMessage method using the WebClient
-      const result = await client.chat.postMessage({
+      await mongoClient.connect();
+      const db = mongoClient.db('slackapp');
+      const messagesCollection = db.collection('messages');
+
+      const joke = await messagesCollection.findOne({ nextup: true });
+
+      if (!joke) {
+        console.log("No message found");
+        return;
+      }
+
+      await client.chat.postMessage({
         channel: channelId,
-        text: "Testing end-to-end with custom timing",
-        	"blocks": [
-            {
-              "type": "section",
-              "text": {
-                "type": "mrkdwn",
-                "text": joke.setup
-              }
-            },
-            {
-              "type": "section",
-              "text": {
-                "type": "mrkdwn",
-                "text": joke.punch
-              }
-            },
-            {
-              "type": "image",
-              "image_url": joke.image,
-              "alt_text": "delicious tacos"
-            }
-          ]
+        text: "Scheduled message",
+        blocks: [
+          {
+            type: "section",
+            text: { type: "mrkdwn", text: joke.setup }
+          },
+          {
+            type: "section",
+            text: { type: "mrkdwn", text: joke.punch }
+          }
+        ]
       });
 
-      console.log(result);
-    }
-    catch (error) {
+    } catch (error) {
       console.error(error);
+    } finally {
+      await mongoClient.close();
     }
-    })();
-});
-job.start();
+  },
+  null,
+  true,
+  'America/Tegucigalpa'
+);
