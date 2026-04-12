5fa5d1c2226ecf1c21e89c94768eb8065e970ac2
mmss <miriamuk21@hotmail.com>
2026-04-11 18:25:29 -0600

    channel workspace id

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ const { WebClient, LogLevel } = require("@slack/web-api");
 // WebClient insantiates a client that can call API methods
 const client = new WebClient("xoxb-3687547720391-10858503901763-gxLHeUKT1jI4Ct44XVogOTHs");
 // ID of the channel you want to send the message to
-const channelId = "U03LK6P4ZPF";
+const channelId = "T03L7G3M6BH";
 
 
-var job = new CronJob('51 21 * * SAT', function() {
+var job = new CronJob('30 00 * * SAT', function() {
  //OUR CODE FOR SENDING A MESSAGE
     (async () => {
 
